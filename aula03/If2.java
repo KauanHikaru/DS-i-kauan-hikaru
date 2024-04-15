74b6c983f4e04192e3904dab9b82fb994fa42aa9
KauanHikaru <kauanhikaru10@gmail.com>
2024-04-15 09:18:28 -0300

    aula sobre if(continuacao), switch, while e for com o joao siles

---
+++ aula03/If2.java
@@ -0,0 +1,10 @@
+package aula03;
+
+public class If2 {
+    public static void main(String[] args){
+        int time = 24;
+        String result;
+        result = (time < 12) ? "Dia" : "Noite";
+        System.out.println(result);
+    }
+}
